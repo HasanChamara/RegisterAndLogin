7fcb81a201322504715bcdbef8f6e274031a6639
IT20157296 <hasanchamara1234@gmail.com>
2021-12-25 16:20:39 +0530

    Save Firebase
    Save user Login Data to Firebase

--- app/src/main/java/com/example/registerandlogin/Register.java
+++ app/src/main/java/com/example/registerandlogin/Register.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,12 +15,20 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
 
-    EditText registerFullName, registerEmail, registerPassword, confPassword;
+    public static final String TAG = "TAG";
+    EditText registerFullName, registerEmail, registerPassword, confPassword, phoneNumber;
     Button registerBtn, goToLoginBtn;
     FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +41,10 @@ public class Register extends AppCompatActivity {
         confPassword = findViewById(R.id.confPassword);
         registerBtn = findViewById(R.id.registerBtn);
         goToLoginBtn = findViewById(R.id.goToLoginBtn);
+        phoneNumber = findViewById(R.id.registerPhoneNum);
 
         fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
 
         goToLoginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,6 +61,7 @@ public class Register extends AppCompatActivity {
 
                 String fullName = registerFullName.getText().toString();
                 String email = registerEmail.getText().toString();
+                String phone = phoneNumber.getText().toString();
                 String password = registerPassword.getText().toString();
                 String ConfPassword = confPassword.getText().toString();
 
@@ -60,6 +72,10 @@ public class Register extends AppCompatActivity {
 
                 if (email.isEmpty()){
                     registerEmail.setError("Email IS Required");
+                    return;
+                }
+                if (phone.isEmpty()){
+                    phoneNumber.setError("Phone Number is Required");
                     return;
                 }
                 if (password.isEmpty()){
@@ -86,6 +102,23 @@ public class Register extends AppCompatActivity {
                     @Override
                     public void onSuccess(AuthResult authResult) {
                         // send user to next page
+                        userID = fAuth.getCurrentUser().getUid();
+                        DocumentReference documentReference = fStore.collection("users").document(userID);
+                        Map<String, Object> user = new HashMap<>();
+                        user.put("fName", fullName);
+                        user.put("email", email);
+                        user.put("phone", phone);
+                        documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Log.d(TAG, "onSuccess: User Profile Created for"+ userID);
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.d(TAG, "onFailure" + e.toString());
+                            }
+                        });
                         startActivity(new Intent(getApplicationContext(), MainActivity.class));
                         finish();
                     }
